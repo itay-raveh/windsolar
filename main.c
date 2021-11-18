a918ab23a76da07b4f328ac10ecf4faae718723d
itay-raveh <itayitay30@gmail.com>
2021-11-18 22:37:00 +0000

    Verify command line args

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "utils.h"
+#define DEBUG 1
+#include "debug.h"
 
-int main()
+
+char *usage = "Usage: windsolar FILE\n"
+              "https://github.com/itay-raveh/windsolar\n";
+
+int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
-    return 0;
+    if (argc != 2) print_and_exit(usage, EXIT_FAILURE);
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) print_and_exit(usage, EXIT_SUCCESS);
+
+    char* fname = argv[1];
+
+    if (access(fname, R_OK) != 0) print_and_exit("Error: No permissions to read file", EXIT_FAILURE);
+
+    debug("using file: %s", fname);
 }
