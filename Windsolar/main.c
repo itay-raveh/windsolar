9ac3a0e2163b818f2d441eb2da0dc1c52d7dc623
itay-raveh <itayitay30@gmail.com>
2021-11-21 22:12:28 +0000

    Use EXIT_WITH_MSG rather then print_and_exit

--- Windsolar/main.c
+++ Windsolar/main.c
@@ -1,4 +1,5 @@
 #define DEBUG
+
 #include <unistd.h>
 #include <string.h>
 #include "utils.h"
@@ -10,13 +11,13 @@ char *usage = "Usage: windsolar FILE\n"
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) print_and_exit(usage, EXIT_FAILURE);
-    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) print_and_exit(usage, EXIT_SUCCESS);
+    if (argc != 2) EXIT_WITH_MSG(EXIT_FAILURE, "%s", usage);
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) EXIT_WITH_MSG(EXIT_SUCCESS, "%s", usage);
 
-    char* fname = argv[1];
+    char *fname = argv[1];
 
-    if (access(fname, F_OK) != 0) print_and_exit("Error: File does not exist", EXIT_FAILURE);
-    if (access(fname, R_OK) != 0) print_and_exit("Error: No permissions to read file", EXIT_FAILURE);
+    if (access(fname, F_OK) != 0) EXIT_WITH_MSG(EXIT_FAILURE, "Error: File '%s' does not exist\n", fname);
+    if (access(fname, R_OK) != 0) EXIT_WITH_MSG(EXIT_FAILURE, "Error: No permissions to read file '%s'\n", fname);
 
     TRACE("using file: %s\n", fname);
 }
