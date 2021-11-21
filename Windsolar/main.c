07627f623c500e64d52d8991655d4bb433997200
itay-raveh <itayitay30@gmail.com>
2021-11-21 22:21:12 +0000

    Clean up main

--- Windsolar/main.c
+++ Windsolar/main.c
@@ -11,13 +11,22 @@ char *usage = "Usage: windsolar FILE\n"
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) EXIT_WITH_MSG(EXIT_FAILURE, "%s", usage);
-    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) EXIT_WITH_MSG(EXIT_SUCCESS, "%s", usage);
+    // ~~~~~~~~~~~~~~~~~ get filename ~~~~~~~~~~~~~~~~~
+
+    if (argc != 2)
+        EXIT_WITH_MSG(EXIT_FAILURE, "%s", usage);
+
+    // if argument is '--help' or '-h'
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
+        EXIT_WITH_MSG(EXIT_SUCCESS, "%s", usage);
 
     char *fname = argv[1];
 
-    if (access(fname, F_OK) != 0) EXIT_WITH_MSG(EXIT_FAILURE, "Error: File '%s' does not exist\n", fname);
-    if (access(fname, R_OK) != 0) EXIT_WITH_MSG(EXIT_FAILURE, "Error: No permissions to read file '%s'\n", fname);
+    if (access(fname, F_OK) != 0)
+        EXIT_WITH_MSG(EXIT_FAILURE, "Error: File '%s' does not exist\n", fname);
+
+    if (access(fname, R_OK) != 0)
+        EXIT_WITH_MSG(EXIT_FAILURE, "Error: No permissions to read file '%s'\n", fname);
 
     TRACE("using file: %s\n", fname);
 }
