8b5d7de38e5964c739a0c99a1dd152784486cb4f
itay-raveh <itayitay30@gmail.com>
2021-11-24 19:44:49 +0000

    Add docs

--- Windsolar/utils.h
+++ Windsolar/utils.h
@@ -7,6 +7,12 @@
 
 #include <stdlib.h>
 
+/**
+ * Safe malloc. will exit with error when allocation failes
+ *
+ * @param size - size of new memory block
+ * @return ptr to memory block
+ */
 void *malloc_s(size_t size);
 
 #endif //WINDSOLAR_UTILS_H
