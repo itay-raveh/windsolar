99256b00c414e2a7bcc56037d8d1009f62602869
itay-raveh <itayitay30@gmail.com>
2021-11-24 20:36:47 +0000

    Prettier trace

--- Windsolar/macros.h
+++ Windsolar/macros.h
@@ -10,7 +10,7 @@
 #define EXIT_WITH_MSG(code, fmt, ...) do{fprintf(code ? stderr : stdout, fmt, __VA_ARGS__);exit(code);}while(0)
 
 #ifdef DEBUG
-#define TRACE(fmt, ...) do{fprintf(stderr, "%s:%s(%4u) " fmt, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);}while(0)
+#define TRACE(fmt, ...) do{fprintf(stderr, "[%s:%s\t:%3u]\t" fmt, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);}while(0)
 #else
 #define TRACE(fmt, ...)
 #endif
