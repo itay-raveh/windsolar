4c5ca8f831f537e7264190143a421424c76b1565
itay-raveh <itayitay30@gmail.com>
2021-11-24 19:45:43 +0000

    Add file stats, some docs, and use FileReader

--- Windsolar/main.c
+++ Windsolar/main.c
@@ -1,7 +1,9 @@
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include "utils.h"
 #include "macros.h"
+#include "file_reader.h"
 
 
 char *usage = "Usage: windsolar FILE\n"
@@ -9,7 +11,7 @@ char *usage = "Usage: windsolar FILE\n"
 
 int main(int argc, char *argv[])
 {
-    // ~~~~~~~~~~~~~~~~~ get filename ~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~ parse args ~~~~~~~~~~~~~~~~~~
 
     if (argc != 2)
         EXIT_WITH_MSG(EXIT_FAILURE, "%s", usage);
@@ -20,11 +22,32 @@ int main(int argc, char *argv[])
 
     char *fname = argv[1];
 
+    // ~~~~~~~~~~~~~~~~~ verify file ~~~~~~~~~~~~~~~~~~
+    TRACE("got file %s\n", fname);
+
+    // if the file does not exist
     if (access(fname, F_OK) != 0)
         EXIT_WITH_MSG(EXIT_FAILURE, "Error: File '%s' does not exist\n", fname);
 
+    // if there are no read permissions for the file
     if (access(fname, R_OK) != 0)
         EXIT_WITH_MSG(EXIT_FAILURE, "Error: No permissions to read file '%s'\n", fname);
 
-    TRACE("using file: %s\n", fname);
+    // ~~~~~~~~~~~~~~~~ get file stats ~~~~~~~~~~~~~~~~
+    TRACE("stating %s\n", fname);
+
+    struct stat sb;
+
+    // if stats could not be retrieved
+    if (stat(fname, &sb) == -1)
+        EXIT_WITH_MSG(EXIT_FAILURE, "Error: Failed to get stats on %s\n", fname);
+
+    // if the file is not a regular file
+    if (!S_ISREG(sb.st_mode))
+        EXIT_WITH_MSG(EXIT_FAILURE, "Error: %s is not a regular file\n", fname);
+
+    FileReader *fr = FileReader_init(fname, sb.st_size);
+    
+
+    FileReader_free(fr);
 }
