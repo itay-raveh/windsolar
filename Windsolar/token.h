3b7c68317de5dccfa3bd5acbe39c6233d0159f53
itay-raveh <itayitay30@gmail.com>
2021-12-08 22:45:01 +0000

    Token is not just an enum, parsing will be in the, well, parser

--- Windsolar/token.h
+++ Windsolar/token.h
@@ -7,88 +7,9 @@
 
 typedef enum
 {
-    PLUS        // +
-} action_t;
-
-typedef enum
-{
-    NAME,       // MAIN etc
-    ACTION,     // action_t
-    LPAR,       // (
-    RPAR,       // )
-    SEMICOL,    // ;
-    NUM_LIT,    // 5 etc
-    STR_LIT     // "Hello World" etc
-} token_type_t;
-
-typedef union
-{
-    char chr;       // LPAR, RPAR, SEMICOL
-    char *str;      // NAME or STR_LIT
-    int32_t num;    // NUM_LIT
-    action_t act;
-} token_content_t;
-
-typedef struct
-{
-    token_type_t type;
-    token_content_t content;
+    LPAR, RPAR, SEMICOL, STRING, NUMBER, LABEL, CMD
 } Token;
 
-/**
- * Dynamically allocate a new Token and initialize the fields.
- *
- * @param type
- * @param content
- * @return ptr to Token
- */
-Token *Token_init(token_type_t type, token_content_t content);
-
-
-/**
- * A string representation of a token
- * @param t - Token
- * @return string describing token
- */
-char *Token_str(Token *t);
-
-/**
- * Free a dynamically allocated Token
- *
- * @param t - Token to free
- */
-void Token_free(Token *t);
-
-/**
- * Check the type of a Token to determine if it's content is a char
- *
- * @param t - Token
- * @return 1 if it is, otherwise 0
- */
-int Token_isContentChr(Token *t);
-
-/**
- * Check the type of a Token to determine if it's content is a string
- *
- * @param t - Token
- * @return 1 if it is, otherwise 0
- */
-int Token_isContentStr(Token *t);
-
-/**
- * Check the type of a Token to determine if it's content is a number
- *
- * @param t - Token
- * @return 1 if it is, otherwise 0
- */
-int Token_isContentNum(Token *t);
-
-/**
- * Check the type of a Token to determine if it's content is an action
- *
- * @param t - Token
- * @return 1 if it is, otherwise 0
- */
-int Token_isContentAction(Token *t);
+extern const char *const token_names[7];
 
 #endif //WINDSOLAR_TOKEN_H
