df07bfbf8b1ec7aeb9abedcaa4dafe8bf61160f1
itay-raveh <itayitay30@gmail.com>
2022-01-02 15:39:14 +0000

    Separate into DECL and IMPL macros

--- Windsolar/stacks.h
+++ Windsolar/stacks.h
@@ -6,49 +6,19 @@
 #define WINDSOLAR_STACKS_H
 
 #include <stdbool.h>
-#include <stdlib.h> // free()
-#include <assert.h> // assert()
 #include "utils.h"  // NEW(), realloc_s(), malloc_s()
 #include "token.h"
 
-#define DECL_STACK(typename, type) \
+#define STACK_DECL(typename, type) \
     typedef struct { \
-        size_t len;         /* amount of data currently in the stack */ \
-        size_t capacity;    /* amount of data possible in the stack */ \
-        type* data;         /* data array */ \
+        size_t len; \
+        size_t capacity; \
+        type* data; \
     } typename; \
- \
-    typename *typename##_new() { \
-        typename *s = NEW(typename); \
-        s->len = 0; \
-        s->capacity = 1; \
-        s->data = malloc_s(sizeof(type)); \
-        return s; \
-    } \
- \
-    void typename##_free(typename* stck) { \
-        free(stck->data); \
-        free(stck); \
-    } \
- \
-    void typename##_push(typename* stck, type value) { \
-        assert(stck); \
- \
-        if (++(stck->len) > stck->capacity) { \
-            stck->capacity = stck->capacity * 2; \
-            stck->data = realloc_s(stck->data, stck->capacity * sizeof(type)); \
-        } \
- \
-        stck->data[stck->len - 1] = value; \
-    } \
- \
-    type typename##_pop(typename* stck) { \
-        assert(stck); \
-        assert(stck->len > 0); \
- \
-        stck->len--; \
-        return stck->data[stck->len]; \
-    }
+    typename *typename##_new(); \
+    void typename##_free(typename *stck); \
+    void typename##_push(typename *stck, type value); \
+    type typename##_pop(typename *stck);
 
 typedef struct data_frame_t
 {
@@ -57,7 +27,9 @@ typedef struct data_frame_t
     char *str;
 } DataFrame;
 
-DECL_STACK(DataStack, DataFrame)
+STACK_DECL(DataStack, DataFrame)
+
+//void DataStack_print(DataStack *ds, int limit);
 
 typedef struct program_frame_t
 {
@@ -65,6 +37,6 @@ typedef struct program_frame_t
     char *str;
 } ProgramFrame;
 
-DECL_STACK(ProgramStack, ProgramFrame)
+STACK_DECL(ProgramStack, ProgramFrame)
 
 #endif //WINDSOLAR_STACKS_H
