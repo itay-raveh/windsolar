b6a903b397a39cc4bc2729ada2176cc34fc55934
itay-raveh <itayitay30@gmail.com>
2022-01-07 11:59:31 +0000

    Generalize logic

--- Windsolar/args.c
+++ Windsolar/args.c
@@ -20,17 +20,16 @@ Args *Args_fromArgv(uint8_t argc, char *argv[])
 
     for (uint8_t i = 2; i < argc; i++)
     {
-        if (argv[i][0] != '-') return NULL;
         if (strcmp(argv[i], "--print-tokens") == 0 || strcmp(argv[i], "-t") == 0) a->printTokens = true;
-        if (strcmp(argv[i], "--print-parse-tree") == 0 || strcmp(argv[i], "-p") == 0) a->printParseTree = true;
-        if (strcmp(argv[i], "--print-stacks") == 0 || strcmp(argv[i], "-s") == 0)
+        else if (strcmp(argv[i], "--print-parse-tree") == 0 || strcmp(argv[i], "-p") == 0) a->printParseTree = true;
+        else if (strcmp(argv[i], "--print-stacks") == 0 || strcmp(argv[i], "-s") == 0)
         {
             a->printStacks = true;
             if (i > argc - 2) return NULL;
             char *end;
             a->stackPrintLength = strtol(argv[i + 1], &end, 10);
             i++;
-        }
+        } else return NULL;
     }
     return a;
 }
