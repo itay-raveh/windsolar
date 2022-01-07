58bbd2a6007a669923dd746937f681e3d105614b
itay-raveh <itayitay30@gmail.com>
2022-01-07 12:00:38 +0000

    Use string_repr

--- Windsolar/frames.c
+++ Windsolar/frames.c
@@ -4,8 +4,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "frames.h"
 #include "utils.h"  // NEW(), realloc_s(), malloc_s()
+
+char *string_repr(char *src, size_t len, size_t field_width)
+{
+    char *str = (char *) malloc_s(len + 3);
+    size_t new_len = len + 2;
+
+    if (new_len <= field_width) strncpy(&(str[1]), src, len);
+    else
+    {
+        strncpy(&(str[1]), src, field_width);
+        new_len = field_width;
+        str[new_len - 2] = str[new_len - 3] = str[new_len - 4] = '.';
+    }
+
+    str[new_len - 1] = str[0] = '"';
+    str[new_len] = '\0';
+
+    return str;
+}
 
 DataFrame *DataFrame_new(const double *number, char *str)
 {
@@ -28,16 +48,20 @@ void DataFrame_free(DataFrame *df)
     free(df);
 }
 
-void DataFrame_print(DataFrame *df, int16_t field_width)
+void DataFrame_print(DataFrame *df, uint16_t field_width)
 {
     if (df->isNumber)
     {
         if ((int64_t) df->number == df->number)
-            printf("%*ld", field_width * -1, (int64_t) df->number);
+            printf("%*.*ld", field_width * -1, field_width, (int64_t) df->number);
         else
-            printf("%*f", field_width * -1, df->number);
+            printf("%*.*f", field_width * -1, field_width, df->number);
     } else
-        printf("\"%*s\"", (field_width - 2) * -1, df->str);
+    {
+        char *str = string_repr(df->str, strlen(df->str), field_width);
+        printf("%*.*s", field_width * -1, field_width, str);
+        free(str);
+    }
 }
 
 ProgramFrame *ProgramFrame_new(Token type, char *str)
@@ -54,10 +78,14 @@ void ProgramFrame_free(ProgramFrame *pf)
     free(pf);
 }
 
-void ProgramFrame_print(ProgramFrame *pf, int16_t field_width)
+void ProgramFrame_print(ProgramFrame *pf, uint16_t field_width)
 {
     if (pf->type == T_STRING)
-        printf("\"%*s\"", (field_width - 2) * -1, pf->str);
+    {
+        char *str = string_repr(pf->str, strlen(pf->str), field_width);
+        printf("%*.*s", field_width * -1, field_width, str);
+        free(str);
+    }
     else
-        printf("%*s", field_width * -1, pf->str);
+        printf("%*.*s", field_width * -1, field_width, pf->str);
 }
